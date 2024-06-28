d6ecfea5bd6357bd48b7f240128deddb88fb6060
LuckyIntegral <frantsv2004@gmail.com>
2024-06-28 18:02:21 +0200

    chore: basic test

--- linear_regression/predict.py
+++ linear_regression/predict.py
@@ -1,13 +1,24 @@
 '''module to predict the price of a car based on the mileage'''
-
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 def main() -> None:
 	'''entrypoint of the script
 	prompts the user to enter the mileage of the car
 	then predicts the price of the car based on theta0 and theta1 from the model
 	'''
-	print("Hello, World!")
+	try:
+		df = pd.read_csv('data/data.csv')
 
+		user_input = float(input('Enter the mileage of the car: '))
+
+		mpl.use('Qt5Agg')
+		plt.plot(df['km'], df['price'], 'o')
+		plt.show()
+
+	except Exception as e:
+		print(e)
 
 if __name__ == '__main__':
 	main()
--- linear_regression/train.py
+++ linear_regression/train.py
@@ -1,4 +1,8 @@
 '''module to train the model based on the dataset'''
+import pandas as pd
+import numpy as np
+import json
+import sys
 
 
 def main() -> None:
@@ -6,8 +10,12 @@ def main() -> None:
 	trains the model based on the dataset
 	stores the theta0 and theta1 in file model.json
 	'''
-	print("Hello, World!")
-
+	try:
+		df = pd.read_csv('data/data.csv')
+		theta0, theta1 = 0, 0
+		json.dump({'theta0': theta0, 'theta1': theta1}, open('model.json', 'w'))
+	except Exception as e:
+		print(e)
 
 if __name__ == '__main__':
 	main()
