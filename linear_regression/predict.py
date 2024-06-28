24f72684774d65c029db2bc8f5cf1968df6cd5d0
LuckyIntegral <frantsv2004@gmail.com>
2024-06-28 15:25:17 +0200

    chore: make structure for .py files, add requirements.txt and data.csv files

---
+++ linear_regression/predict.py
@@ -0,0 +1,13 @@
+'''module to predict the price of a car based on the mileage'''
+
+
+def main() -> None:
+	'''entrypoint of the script
+	prompts the user to enter the mileage of the car
+	then predicts the price of the car based on theta0 and theta1 from the model
+	'''
+	print("Hello, World!")
+
+
+if __name__ == '__main__':
+	main()
---
+++ linear_regression/train.py
@@ -0,0 +1,13 @@
+'''module to train the model based on the dataset'''
+
+
+def main() -> None:
+	'''entrypoint of the script
+	trains the model based on the dataset
+	stores the theta0 and theta1 in file model.json
+	'''
+	print("Hello, World!")
+
+
+if __name__ == '__main__':
+	main()
