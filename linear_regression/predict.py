243493e25b41064933e93b036cc0d1fb685e4cb8
Vitalik <frantsv2004@gmail.com>
2024-06-30 22:54:45 +0200

    feat: add predict service class for user interaction

--- linear_regression/predict.py
+++ linear_regression/predict.py
@@ -1,24 +1,76 @@
 '''module to predict the price of a car based on the mileage'''
-import pandas as pd
-import matplotlib as mpl
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+import pandas as pd
+import numpy as np
+import json
+
+
+class PredictionService:
+    '''class to predict the price of a car based on the mileage'''
+    def __init__(self) -> None:
+        '''initializes the class with the theta0 and theta1 values'''
+        self.df = None
+        self.theta0, self.theta1 = 0, 0
+        self.users_prediction = 0
+        self.predicted_price = 0
+
+    def start(self) -> None:
+        self.df = pd.read_csv('data/data.csv')
+        self.theta0, self.theta1 = self.getTheta()
+        self.users_prediction = self.getUsersPrediction()
+        self.predicted_price = self.theta0 + self.theta1 * self.users_prediction
+        print(f'The predicted price of the car is: {self.predicted_price}$')
+        self.plotData()
+
+    def getTheta(self) -> tuple:
+        '''returns the theta0 and theta1 values from the model'''
+        data = json.load(open('model.json'))
+        theta0 = data['theta0']
+        theta1 = data['theta1']
+        return theta0, theta1
+
+    def getUsersPrediction(self) -> float:
+        '''returns the predicted price of the car based on the mileage'''
+        number = float(input('Enter the mileage of the car: '))
+        if number < 0:
+            raise ValueError('Mileage cannot be negative')
+        return number
+
+    def plotData(self) -> None:
+        '''plots the data points in the data.csv file'''
+        # mpl.use('Qt5Agg') # needed for virtual environment
+        line = np.array([0, - self.theta0 / self.theta1])
+        plt.plot(line, self.theta0 + self.theta1 * line, 'r')
+        plt.plot(self.df['km'], self.df['price'], 'o')
+        plt.plot(self.users_prediction, self.predicted_price, 'go')
+
+        plt.legend(['Model', 'Data points', 'User prediction'])
+        plt.title('Price of cars based on mileage')
+
+        plt.xlabel('Mileage (in thousands of km)')
+        plt.gca().get_xaxis().set_major_formatter(
+            plt.FuncFormatter(lambda x, _: f'{int(x / 1000)}')
+        )
+
+        plt.ylabel('Price')
+        plt.gca().get_yaxis().set_major_formatter(
+            plt.FuncFormatter(lambda y, _: f'{int(y / 1000)}k')
+        )
+
+        plt.show()
+
 
 def main() -> None:
-	'''entrypoint of the script
-	prompts the user to enter the mileage of the car
-	then predicts the price of the car based on theta0 and theta1 from the model
-	'''
-	try:
-		df = pd.read_csv('data/data.csv')
-
-		user_input = float(input('Enter the mileage of the car: '))
-
-		mpl.use('Qt5Agg')
-		plt.plot(df['km'], df['price'], 'o')
-		plt.show()
-
-	except Exception as e:
-		print(e)
+    '''entrypoint of the script
+    prompts the user to enter the mileage of the car
+    then predicts the price of the car based on theta0 and theta1 from the model
+    '''
+    try:
+        prediction_service = PredictionService()
+        prediction_service.start()
+    except Exception as e:
+        print(e)
 
 if __name__ == '__main__':
-	main()
+    main()
